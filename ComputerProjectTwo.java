8815bd1460df4225940340845b5444652093b06e
Ariel-maxcoder <61765883+Ariel-maxcoder@users.noreply.github.com>
2021-09-11 15:21:04 -0400

    Add files via upload

---
+++ ComputerProjectTwo.java
@@ -0,0 +1,57 @@
+
+import java.util.Scanner;
+
+public class ComputerProjectTwo {
+	
+	public static boolean myApp(boolean a, boolean b)
+	{
+		if(a)
+		{return b;}
+		else
+		{return true;}
+	} 
+	
+	public static boolean conJ(boolean a, boolean b)
+	{
+		return a && b;
+	}
+	
+	public static boolean disJ(boolean a, boolean b)
+	{
+		return a || b;
+	}
+	
+	public static boolean Bic(boolean a, boolean b) 
+	{
+		return !!a == !!b;
+	}
+	
+	public static boolean Xor(boolean a, boolean b)
+	{
+		return (a || b) && !(a && b);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		boolean p , q;
+		 Scanner sc=new Scanner(System.in);
+		System.out.print("Type in true or false for 'p': ");
+		p = sc.nextBoolean();
+		
+		System.out.print("Type in true or false for 'q': " );
+		q = sc.nextBoolean();
+		
+		
+		
+		System.out.println( "P\tQ\tCONJ\tDISJ\tCON\tBIC\tXOR");
+
+		
+		
+		 System.out.println(p + "\t" + q + "\t" + conJ(p,q) + "\t" + disJ(p,q) + "\t" + myApp(p,q) + "\t" + Bic(p,q) + "\t" + Xor(p,q)); 
+		 
+		
+		
+	}
+
+}
