c92875342929d339fa74e322e7f5e777f9e5a0ac
Ariel-maxcoder <61765883+Ariel-maxcoder@users.noreply.github.com>
2021-09-11 20:21:43 -0400

    Add files via upload

---
+++ ComputerProject Version 2 (Junit).java
@@ -0,0 +1,60 @@
+package com.company;
+import org.junit.Test;
+
+import java.util.Scanner;
+
+public class ComputerProjectTwo {
+
+    @Test
+    public static boolean myApp(boolean a, boolean b)
+    {
+        if(a)
+        {return b;}
+        else
+        return true;
+        }
+
+        @Test
+        public static boolean conJ(boolean a, boolean b)
+        {
+        return a && b;
+        }
+
+        @Test
+        public static boolean disJ(boolean a, boolean b)
+        {
+        return a || b;
+        }
+
+        @Test
+        public static boolean Bic(boolean a, boolean b)
+        {
+        return !!a == !!b;
+        }
+
+        @Test
+        public static boolean Xor(boolean a, boolean b)
+        {
+        return (a || b) && !(a && b);
+        }
+
+    public static void main(String[] args) {
+
+        boolean p, q;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Type in true or false for 'p': ");
+        p = sc.nextBoolean();
+
+        System.out.print("Type in true or false for 'q': " );
+        q = sc.nextBoolean();
+
+        System.out.println( "P\t\tQ\t\tCONJ\tDISJ\tCON\t\tBIC\t\tXOR");
+
+
+
+        System.out.println(p + "\t" + q + "\t" + conJ(p,q) + "\t" + disJ(p,q) + "\t" + myApp(p,q) + "\t" + Bic(p,q) + "\t" + Xor(p,q));
+
+
+
+    }
+}
