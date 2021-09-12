0b148e8119a9287bb6acf3db234f6f53833ed1e9
Ariel-maxcoder <61765883+Ariel-maxcoder@users.noreply.github.com>
2021-09-11 20:17:22 -0400

    Add files via upload

---
+++ ComputerTest Version 1 (Junit).java
@@ -0,0 +1,52 @@
+package com.company;
+
+import org.junit.Test;
+
+public class ComputerTest {
+
+    @Test
+    public static boolean myCon(boolean a, boolean b)
+    {
+        if(a)
+        {return b;}
+        else
+        {return true;}
+    }
+
+    @Test
+    public static void main(String[] args) {
+
+        boolean p, q;
+
+        System.out.println( "P\t\tQ\t\tCONJ\tDISJ\tCON\t\tBIC\t\tXOR");
+
+        p = true; q = true;
+        boolean x = p && q;
+        boolean y = p || q;
+        boolean g = !!p == !!q;
+        boolean u = (p || q) && !(p && q);
+        System.out.println(p + "\t" + q + "\t" + x + "\t" + y + "\t" + myCon(p,q) + "\t" + g + "\t" + u);
+
+        p = true; q = false;
+        boolean j = p && q;
+        boolean s = p || q;
+        boolean l = !!p == !!q;
+        boolean o = (p || q) && !(p && q);
+        System.out.println(p + "\t" + q + "\t" + j + "\t" + s + "\t" + myCon(p,q) + "\t" + l + "\t" + o);
+
+        p = false; q = true;
+        boolean h = p && q;
+        boolean m = p || q;
+        boolean z = !!p == !!q;
+        boolean e = (p || q) && !(p && q);
+        System.out.println(p + "\t" + q + "\t" + h + "\t" + m + "\t" + myCon(p,q) + "\t" + z + "\t" + e);
+
+        p = false; q = false;
+        boolean r = p && q;
+        boolean v = p || q;
+        boolean f = !!p == !!q;
+        boolean w = (p || q) && !(p && q);
+        System.out.println(p + "\t" + q + "\t" + r + "\t" + v + "\t" + myCon(p,q) + "\t" + f + "\t" + w);
+
+    }
+}
